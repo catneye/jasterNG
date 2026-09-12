4919a5d0551db9cda410dade3d2bfa507dceaf53
olegusp@yandex.ru <olegusp@yandex.ru>
2026-09-12 19:46:12 +0300

    fix for GPT

--- src/main/java/com/catneye/asterisk/utils/DateUtils.java
+++ src/main/java/com/catneye/asterisk/utils/DateUtils.java
@@ -1,8 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * jasterNG - Java interface for Asterisk
+ *
+ * Copyright (C) 2026 Kurchenko O.E.
+ *
+ * This file is part of jasterNG.
+ *
+ * jasterNG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, version 2.
+ *
+ * jasterNG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jasterNG. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Modified by Kurchenko O.E. 2026-09-12.
  */
+
 package com.catneye.asterisk.utils;
 
 import com.catneye.asterisk.Exception.WrongDateException;
