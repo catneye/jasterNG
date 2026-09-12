7775d0f20a1ee7114c5864bbde44e835f6d01a88
catneye <71065766+catneye@users.noreply.github.com>
2026-09-12 19:35:56 +0300

    Add GPLv2 header to WrongAddressException.java

--- src/main/java/com/catneye/asterisk/Exception/WrongAddressException.java
+++ src/main/java/com/catneye/asterisk/Exception/WrongAddressException.java
@@ -1,13 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * jasterNG - Java interface for Asterisk
+ *
+ * Copyright (C) 2026 Kurchenko O.E.
+ *
+ * This file is part of jasterNG.
+ *
+ * jasterNG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, version 2.
+ *
+ * jasterNG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jasterNG. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Modified by Kurchenko O.E. 2026-09-12.
  */
 package com.catneye.exception;
 
 /**
  *
- * @author Zaleskovskiy
+ * @author Kurchenko O.E.
  */
 public class WrongAddressException  extends RuntimeException { 
     public WrongAddressException(String error) {
